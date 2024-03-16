cbd1abc95f61666342c843759c7280b063001069
Domen Merhar <domen.merhar@students.vegova.si>
2024-03-16 07:42:02 +0100

    Added buttons and state

--- stopwatch/src/App.tsx
+++ stopwatch/src/App.tsx
@@ -1,7 +1,32 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [time, setTime] = useState<number>(0);
+  const [running, setRunning] = useState<boolean>(false);
+
+  return (
+    <>
+      <h1>Stopwatch</h1>
+      <div>
+        <span>{"0" + Math.floor((time / 60000) % 60)}</span>
+        <span>{"0" + Math.floor((time / 1000) % 60)}</span>
+        <span>{"0" + ((time / 10) % 100)}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button
+          onClick={() => {
+            setTime(0);
+            setRunning(false);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
